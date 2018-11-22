c09645a9495b52ebbf429bc45afeb77911044c2f
Daniel Ron <daniel.ron@jobsity.io>
2018-11-22 09:55:04 -0500

    Resolve newYearChaous challenge

--- src/main/java/com/nagarosoft/app/dojo/arrays/newyearchaos/NewYearChaos.java
+++ src/main/java/com/nagarosoft/app/dojo/arrays/newyearchaos/NewYearChaos.java
@@ -1,21 +1,40 @@
 package com.nagarosoft.app.dojo.arrays.newyearchaos;
 
-import java.util.Arrays;
-
+/**
+ * New Year Chaos
+ *
+ * @see <a href="https://www.hackerrank.com/challenges/new-year-chaos/problem">Hackerrank - New Year Chaos</a>
+ */
 public class NewYearChaos {
 
+    /**
+     * Minimum Brides
+     *
+     * @param queue
+     * @return
+     */
     public String minimumBribes(int[] queue) {
-        int[] diff = new int[queue.length];
+        int numBribes = 0;
 
-        for (int i = 0; i < queue.length; i++) {
-            diff[i] = Math.abs(queue[i] - (i + 1));
+        for(int index = queue.length - 2; index >= 0; index--) {
+            int j = index + 1;
+            int element = queue[index];
+            int movements = 0;
+
+            while( j < queue.length && element > queue[j] ) {
+                queue[j-1] = queue[j];
+                j++;
+                movements++;
+
+                if( movements > 2 )
+                    return "Too chaotic";
+            }
+
+            queue[j-1] = element;
+            numBribes += movements;
         }
 
-        if(Arrays.stream(diff).anyMatch(it -> it > queue.length / 2)) {
-            return "Too chaotic";
-        } else {
-            return String.valueOf(new Double(Arrays.stream(diff).sum() / 2).intValue());
-        }
+        return Integer.toString(numBribes);
     }
 
 }
